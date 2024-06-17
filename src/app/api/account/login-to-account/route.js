9a623d8d2fa4c0a097497eb9fd9e74386de2f28c
blank0826 <aditya26052002@gmail.com>
2024-06-17 14:44:00 +0530

    Creating Streamify

--- src/app/api/account/login-to-account/route.js
+++ src/app/api/account/login-to-account/route.js
@@ -1,21 +1,22 @@
-import { NextResponse } from "next/server";
 import connectToDB from "@/database";
 import Account from "@/models/Account";
 import { compare } from "bcryptjs";
+import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
 export async function POST(req) {
   try {
     await connectToDB();
-    const { pin, account, uid } = await req.json();
+
+    const { pin, accountId, uid } = await req.json();
 
     const getCurrentAccount = await Account.findOne({ _id: accountId, uid });
 
     if (!getCurrentAccount) {
       return NextResponse.json({
         success: false,
-        message: "Account Not Found!",
+        message: "Account not found",
       });
     }
 
@@ -24,19 +25,18 @@ export async function POST(req) {
     if (checkPin) {
       return NextResponse.json({
         success: true,
-        message: "Welcome to StreamHaven!",
+        message: "Welcome to Netflix!",
       });
     } else {
       return NextResponse.json({
         success: false,
-        message: "Incorrect Pin!",
+        message: "Incorrect PIN ! Please try again",
       });
     }
   } catch (e) {
-    console.log(e);
     return NextResponse.json({
       success: false,
-      message: "Something Went Wrong",
+      message: "Something Went wrong",
     });
   }
 }
